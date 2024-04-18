25acb8b457887f6ad3ae4b9d7423f74762299bf2
usuario <paumirallesdiaz@gmail.com>
2024-04-18 11:05:45 +0200

    BDD Creada y enlazada

---
+++ src/api/api.js
@@ -0,0 +1,12 @@
+export const obtenerJuegos = async () => {
+    try {
+        const response = await fetch('http://localhost/juegos.php/juegos');
+        if (!response.ok) {
+            throw new Error(`Error al obtener la lista de juegos: ${response.status} ${response.statusText}`);
+        }
+        return await response.json();
+    } catch (error) {
+        console.error('Error al obtener juegos:', error);
+        return [];
+    }
+};
--- src/pages/Body.jsx
+++ src/pages/Body.jsx
@@ -1,125 +1,67 @@
-import React, { useState } from 'react'
-
-import { Grid } from '@mui/material';
-
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-
-import '../styles/body.css'
+import React, { useState, useEffect } from 'react';
+import { Grid, Card, CardActions, CardContent, CardMedia, Button, Typography } from '@mui/material';
+import { obtenerJuegos } from '../api/api';
+import '../styles/body.css';
 
 const Body = () => {
+    const [games, setGames] = useState([]);
 
-    const [openDialog, setOpenDialog] = useState();
+    useEffect(() => {
+        cargarJuegos();
+    }, []);
 
-    const videogames = [
-        {
-            "title": "Outer Wilds",
-            "description": "Outer Wilds es un juego de mundo abierto donde el misterio gira en torno a un sistema solar atrapado en un bucle de tiempo",
-            "date": "28/05/2019",
-            "creator": "Marijn Haverbeke",
-            "image": "img/outer-wilds.png",
-            "platform": "PC, PlayStation, Xbox y Switch",
-            "genre": "",
-            "trailer": "https://www.youtube.com/watch?v=LP1RN9Xg5lQ&pp=ygUZZmlyc3QgdHJhaWxlciBvdXRlciB3aWxkcw%3D%3D"
-        },
-        {
-            "title": "Cuphead",
-            "description": "Eloquent JavaScript, Second Edition",
-            "date": "XX/XX/XXXX",
-            "creator": "Studio MDHR",
-            "image": "img/cuphead.png",
-            "platform": "PC, PlayStation, Xbox y Switch",
-            "genre": "",
-            "trailer": "https://www.youtube.com/watch?v=v80_FwbAv_s&pp=ygUVZmlyc3QgdHJhaWxlciBjdXBoZWFk"
-        },
-        {
-            "title": "Ori And The Will Of The Wisps",
-            "description": "",
-            "date": "XX/XX/XXXX",
-            "creator": "Moon Studios",
-            "image": "img/oriATWOTW.png",
-            "platform": "PC, Xbox y Switch",
-            "genre": "",
-            "trailer": "https://www.youtube.com/watch?v=2kPSl2vyu2Y&pp=ygUrZmlyc3QgdHJhaWxlciBPcmkgQW5kIFRoZSBXaWxsIE9mIFRoZSBXaXNwcw%3D%3D"
-        },
-        {
-            "title": "Little Nightmares 2",
-            "description": "",
-            "date": "XX/XX/XXXX",
-            "creator": "Tarsier Studio",
-            "image": "img/littleNightmares2.png",
-            "platform": "PC, PlayStation, Xbox y Switch",
-            "genre": "",
-            "trailer": "https://www.youtube.com/watch?v=quP1qzwFGas&pp=ygUhZmlyc3QgdHJhaWxlciBsaXR0bGUgbmlnaHRtYXJlcyAy"
-        },
-        {
-            "title": "Sea Of Stars",
-            "description": "Sea of ​​Stars cuenta la historia de dos Hijos del Solsticio que combinarán los poderes del sol y la luna para realizar la Magia del Eclipse.",
-            "date": "29/08/2023",
-            "creator": "Sabotage Studio",
-            "image": "../img/seaofStars.png",
-            "platform": "PC, PlayStation, Xbox y Switch",
-            "genre": "Aventura, Indie, Rol",
-            "trailer": "https://www.youtube.com/watch?v=LP1RN9Xg5lQ&pp=ygUZZmlyc3QgdHJhaWxlciBvdXRlciB3aWxkcw%3D%3D"
-        },
-        {
-            "title": "The Last Of Us 2",
-            "description": "Naughty Dogs",
-            "date": "28/05/XXXX",
-            "creator": "Marijn Haverbeke",
-            "image": "MyGotys-Final/src/img/TLOU2.png",
-            "platform": "PlayStation",
-            "genre": "",
-            "trailer": "https://www.youtube.com/watch?v=LP1RN9Xg5lQ&pp=ygUZZmlyc3QgdHJhaWxlciBvdXRlciB3aWxkcw%3D%3D"
+    const cargarJuegos = async () => {
+        try {
+            const juegos = await obtenerJuegos();
+            console.log(juegos); // Verificar en la consola los juegos obtenidos
+            setGames(juegos);
+        } catch (error) {
+            console.error('Error al cargar juegos:', error);
         }
-    ];
+    };
 
     return (
         <div className='container-body'>
-            {/* //TODO Mirar que tamaño pongo para Responsive */}
-            {/* //TODO manejar el maxWidth en una clase de CSS para manejar todas las cards */}
-            {/* //TODO Array de una Card para que salgan todas */}
             <Grid container spacing={2} className='grid-container'>
-                {videogames.map((game, index) => (
-                    <Grid key={index} item xs={12} sm={12} md={6} lg={4}>
-                        <Card className='card' style={{ height: '300px' }}>
-                            <div className="card-image">
-                                <CardMedia
-                                    component="img"
-                                    alt={game.title}
-                                    height="140"
-                                    // src={game.image}
-                                    image={game.image}
-                                />
-                            </div>
-                            <div className="card-content">
-                                <CardContent>
-                                    <Typography gutterBottom variant="h7" component="div">
-                                        {game.title}
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary">
-                                        {game.creator}
-                                    </Typography>
-                                </CardContent>
-                            </div>
-                            <div className="card-buttons">
-                                <CardActions style={{ justifyContent: 'space-evenly' }}>
-                                    <Button variant="outlined" size="small">Trailer</Button>
-                                    <Button variant="outlined" size="small">Descripción</Button>
-                                    {/* onClick={setOpenDialog(true)} */}
-                                    <Button variant="outlined" size="small">Comentarios</Button>
-                                </CardActions>
-                            </div>
+                {games.map((game) => (
+                    <Grid key={game.id} item xs={12} sm={12} md={6} lg={4}>
+                        <Card>
+                            {/* Mostrar la imagen del juego */}
+                            <CardMedia
+                                component="img"
+                                alt={game.title}
+                                // height="140"
+                                image={`data:image/jpeg;base64,${game.image}`} // Coloca la imagen base64 como fuente
+                            />
+                            {/* Contenido de la tarjeta */}
+                            <CardContent>
+                                <Typography gutterBottom variant="h6" component="div">
+                                    {game.title}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Creador: {game.creator}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Plataforma: {game.platform}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Género: {game.genre}
+                                </Typography>
+                            </CardContent>
+                            {/* Acciones de la tarjeta (botones) */}
+                            {/* //TODO Grid container para cada botón*/}
+                            <CardActions>
+                                <Button variant="outlined" size="small" onClick={() => console.log(`Trailer de ${game.title}`)}>Trailer</Button>
+                                <Button variant="outlined" size="small" onClick={() => console.log(`Descripción de ${game.title}`)}>Descripción</Button>
+                                {/* Puedes implementar la lógica para mostrar comentarios */}
+                                <Button variant="outlined" size="small" onClick={() => console.log(`Comentarios de ${game.title}`)}>Comentarios</Button>
+                            </CardActions>
                         </Card>
                     </Grid>
                 ))}
             </Grid>
         </div>
-    )
-}
+    );
+};
 
-export default Body
+export default Body;
